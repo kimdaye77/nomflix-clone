f87da9066860f3f696f2ae2826c1044f55126fdc
kimdaye77 <kimdaye77@naver.com>
2023-10-11 21:34:34 +0900

    [FEAT] routes 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
