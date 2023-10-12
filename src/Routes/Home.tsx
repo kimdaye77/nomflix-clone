81b9233bd1ae8bcb363c4bcf48c3282c640ac6c3
kimdaye77 <kimdaye77@naver.com>
2023-10-12 22:01:21 +0900

    [FIX] 빠르게 클릭 시 사라지는 현상 겹침 버그 해결

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -75,7 +75,14 @@ function Home() {
     getMovies
   );
   const [index, setIndex] = useState(0);
-  const increaseIndex = () => setIndex((prev) => prev + 1);
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    if (leaving) return;
+    toggleLeaving();
+    setIndex((prev) => prev + 1);
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+
   return (
     <Wrapper>
       {isLoading ? (
@@ -90,7 +97,7 @@ function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence>
+            <AnimatePresence onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVariants}
                 transition={{ type: "tween", duration: 1 }}
